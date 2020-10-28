2123dd78a46ddf4eab06a1842c4a3084f7d8db32
sniperwolf0 <ucancallmesan@gmail.com>
2020-10-28 14:51:59 +0530

    1st commit

---
+++ datatype.js
@@ -0,0 +1,25 @@
+//String, Numbers, boolean, null, undefined
+
+const name ='sam';
+const number =20;
+const rating=8.9;
+const thisiscool =true;
+const hi =null;
+const hii =undefined;
+let z;
+
+
+console.log(typeof name);      //string
+console.log(typeof number);    //number
+console.log(typeof rating);    //number no such things decimal in java script
+console.log(typeof thisiscool);//boolean
+console.log(typeof hi);        //object
+console.log(typeof hii);       //undefined
+console.log(typeof z);         //undefined
+
+
+/**================================================================================================
+ **                                         String
+ *================================================================================================**/
+
+ 
---
+++ variable.js
@@ -0,0 +1,21 @@
+                 //variable
+                 // var, let, const
+                 //var - we can set a variable globally.
+
+                 //(globally scope,ex -let we have a condition and we set a variable.out side of that block if we have another variable with same name that can cause a issue thats why rare use.)
+
+                 //let and const were add in es6(es2015)es-ecmo script
+
+                 //let-wit this we can re assign value
+                 //ex-
+let age =28;
+age=30;          //we can modify here
+console.log(age);
+
+                 //const - A constant is a value that cannot be altered by the program during normal execution. It cannot change through re-assignment, and it can't be redeclared.
+const hi =28;
+hi=40;           //can't change
+const hi=40;     //can't change
+console.log(hi);
+
+                 //use const when you know and sure your gone re assign vale.its very robust and secure.
