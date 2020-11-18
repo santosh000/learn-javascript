8c21f793272a42bc87c21a7df17a8ccfa453d5cd
sniperwolf0 <ucancallmesan@gmail.com>
2020-11-18 23:07:30 +0530

    minor edits

---
+++ DOM/main.js
@@ -0,0 +1,9 @@
+// 1st of all lets see how many windows object
+
+console.log(window);  // u can see in console the list of object that windows have.
+
+// singe element  (grab a single element)
+
+console.log( document.getElementById('my-form')); //grab my-form id from index.html
+
+// Multiple element  (grab multiple elements)
